99cb7b615b02ff0e71cddea6a3415a8d575cbea0
Obaro <obaro.ogbo@gmail.com>
2019-05-30 16:30:48 +0100

    Initial Commit

---
+++ SpeakUp/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace SpeakUp.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<SpeakUp.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(SpeakUp.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
